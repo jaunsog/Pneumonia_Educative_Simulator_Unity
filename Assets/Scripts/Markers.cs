3440c3797ac709614239fc7e25b97149557cb5aa
jaunsog <psarmientoj@uninorte.edu.co>
2021-04-06 20:40:33 -0500

    Diseño_3D_y_funciones

---
+++ Assets/Scripts/Markers.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Markers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Image iconimg;
+    private Text distanceText;
+
+    public Transform player;
+    public Transform target;
+    public Camera cam;
+    public float closeEnoughDist;
+    void Start()
+    {
+        iconimg = GetComponent<Image>();
+        distanceText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            GetDistance();
+            CheckOnScreen();
+        }
+    }
+    private void GetDistance()
+    {
+        float dist = Vector3.Distance(player.position, target.position);
+        distanceText.text = dist.ToString("f1")+"m";
+
+        if (dist<closeEnoughDist)
+        {
+            ToggleUI(false);
+        }
+    }
+    private void CheckOnScreen()
+    {
+        float thing = Vector3.Dot((target.position - cam.transform.position).normalized, cam.transform.forward);
+        float dist = Vector3.Distance(player.position, target.position);
+        if (thing< 0)
+        {
+            ToggleUI(false);
+        }
+        else if (dist>closeEnoughDist)
+        {
+            ToggleUI(true);
+            transform.position = cam.WorldToScreenPoint(target.position);
+        }
+    }
+    private void ToggleUI(bool _value)
+    {
+        iconimg.enabled =_value;
+        distanceText.enabled =_value;
+    }
+}
---
+++ Assets/Scripts/itemScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, gunContainer, fpsCam;
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+    public bool equipped=false;
+    public static bool slotFull;
+    public Vector3 LastScale;
+    public Vector3 LastPosition;
+    public Quaternion LastRotation;
+    public Transform LastParent;
+    void Start()
+    {
+        if(!equipped)
+        {
+            rb.isKinematic=false;
+            coll.isTrigger = false; 
+        }
+        if(equipped)
+        {
+            rb.isKinematic =true;
+            coll.isTrigger = true;
+            slotFull = true;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if(!equipped && distanceToPlayer.magnitude <= pickUpRange&& Input.GetKeyDown(KeyCode.E) && !slotFull) PickUp();
+
+        if(equipped&& Input.GetKeyDown(KeyCode.Mouse0)) Drop();
+    }
+    private void PickUp()
+    {
+        //No tomar más objetos
+        equipped =true;
+        slotFull = true;
+        //Child container
+        LastPosition=transform.localPosition;
+        LastRotation=transform.localRotation;
+        LastParent=transform.parent;
+        LastScale=transform.localScale;
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        //Crear triggers
+        rb.isKinematic=true;
+        coll.isTrigger=true;
+    }
+    private void Drop()
+    {
+        //No tomar más objetos
+        equipped =false;
+        slotFull = false;
+        //No Child container
+        
+        transform.SetParent(LastParent);
+        //Crear triggers
+        rb.isKinematic=false;
+        coll.isTrigger=false;
+        //Lanzar
+        transform.localPosition=LastPosition;
+        transform.localRotation=LastRotation;
+        transform.localScale=LastScale;
+        //rb.AddForce(fpsCam.forward*dropForwardForce, ForceMode.Impulse);
+        //rb.AddForce(fpsCam.up*dropUpwardForce, ForceMode.Impulse);
+
+    }
+}
